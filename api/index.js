ee23343b9ecd412fa131ce6595f356cfe192f576
Iulian Rotaru (mortimr) <iulian@rotaru.fr>
2021-02-02 14:07:54 +0100

    feat: prevent duplicates

--- api/index.js
+++ api/index.js
@@ -5,40 +5,50 @@ const base = new Airtable({
   apiKey: process.env.AIRTABLE_KEY,
 }).base(process.env.AIRTABLE_BASE);
 
-module.exports = (req, res, next) => {
-
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
+module.exports = (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
   res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
+    "Access-Control-Allow-Methods",
+    "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
 
   if (!isValidEmail(req.query.mail)) {
-    next(new Error('Invalid email'));
+    res.status(500).send("Invalid Email");
   } else {
-  base("waitlist").create(
-    [
-      {
-        fields: {
-          mail: req.query.mail,
-        },
-      },
-    ],
-    function (err, records) {
-      if (err) {
-        console.error(err);
-        next(err);
-        return;
-      }
-      records.forEach(function (record) {
-        console.log(record.getId());
+    base("waitlist")
+      .find(req.query.mail)
+      .then((res) => {
+        if (res) {
+          res.status(500).send("Already registered");
+        } else {
+          base("waitlist").create(
+            [
+              {
+                fields: {
+                  mail: req.query.mail,
+                },
+              },
+            ],
+            function (err, records) {
+              if (err) {
+                console.error(err);
+                res.status(500).send("Error while creating");
+                return;
+              }
+              records.forEach(function (record) {
+                console.log(record.getId());
+              });
+              res.json(records);
+            }
+          );
+        }
       });
-      res.json(records);
-    }
-  );
   }
 };
